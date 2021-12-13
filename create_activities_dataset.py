af011c0be562e9c044c2a8cd37d32af5ddcfe65e
Richard Robbins <RERobbins@iTinker.net>
2021-12-13 08:19:59 -0600

    create activities script and pickle file

--- create_activities_dataset.py
+++ create_activities_dataset.py
@@ -1,9 +1,9 @@
 import json
 import pandas as pd
 
-garmin_directory = "/home/rerobbins/garmin_data/DI_CONNECT/DI-Connect-Fitness"
-json_file_0 = garmin_directory + "/rerobbins_0_summarizedActivities.json"
-json_file_1 = garmin_directory + "/rerobbins_1001_summarizedActivities.json"
+json_directory = "/home/rerobbins/garmin_data/DI_CONNECT/DI-Connect-Fitness"
+json_file_0 = json_directory + "/rerobbins_0_summarizedActivities.json"
+json_file_1 = json_directory + "/rerobbins_1001_summarizedActivities.json"
 
 with open(json_file_0) as f:
     data_0 = json.load(f)
@@ -11,16 +11,14 @@ with open(json_file_0) as f:
 with open(json_file_1) as f:
     data_1 = json.load(f)
 
-df_0 = pd.json_normalize(data_0, record_path="summarizedActivitiesExport")
-df_1 = pd.json_normalize(data_1, record_path="summarizedActivitiesExport")
+raw_df_0 = pd.json_normalize(data_0, record_path="summarizedActivitiesExport")
+raw_df_1 = pd.json_normalize(data_1, record_path="summarizedActivitiesExport")
 
-df = pd.concat([df_0, df_1], join="outer", ignore_index=True)
-raw_df = df.set_index(pd.to_datetime(df["startTimeLocal"], unit="ms"))
+raw_df = pd.concat([raw_df_0, raw_df_1], join="outer", ignore_index=True)
 
-# We will derive avgPaceMPM later, but create a spot for it here.
+# Derive timestamp objects to use as index.
 
-df = raw_df.copy()
-df['avgPaceMPM'] = pd.NA
+raw_df = raw_df.set_index(pd.to_datetime(raw_df["startTimeLocal"], unit="ms"))
 
 # Filter columns to retain.
 
@@ -38,7 +36,6 @@ mask = [
     "distance",
     "avgSpeed",
     "maxSpeed",
-    "avgPaceMPM",
     "maxRunCadence",
     "avgBikeCadence",
     "maxBikeCadence",
@@ -53,9 +50,9 @@ mask = [
     "vigorousIntensityMinutes",
 ]
 
-df = df[mask]
+df = raw_df[mask].copy()
 
-# Filter columns to round to 0 and convert to nullable integers with NA for 0
+# Filter columns to round to 0 and convert to nullable integers with NA for 0.
 
 mask = [
     "movingDuration",
@@ -72,31 +69,37 @@ mask = [
 
 df[mask] = df[mask].round(0).convert_dtypes().replace(0, pd.NA)
 
+# Derive duration fields.
 
 df.duration = pd.to_timedelta(df.duration, unit="ms")
 df.elapsedDuration = pd.to_timedelta(df.elapsedDuration, unit="ms")
 df.movingDuration = pd.to_timedelta(df.movingDuration, unit="ms")
 
-# Distance seems to be recorded in centimeters.
-# One mile has 160934.4 centimeters
+# Distance in centimeters, one mile has 160934.4 centimeters.
 
 df.distance = df.distance / 160934.3
 df.rename(columns={"distance": "distanceMiles"}, inplace=True)
 
-# Speed (at least for cycling) seems to be recorded in dekameters per second.
+# Speed is recorded in dekameters per second.
 # One dekameter per second is 22.369363 miles per hour.
 
 df.avgSpeed = df.avgSpeed * 22.369363
 df.maxSpeed = df.maxSpeed * 22.369363
+
 df.rename(columns={"avgSpeed": "avgSpeedMPH", "maxSpeed": "maxSpeedMPH"},
           inplace=True)
 
-# Pace in minutes per mile is 60/MPH, only set it where avgSpeedMPH > 0
+# Add a column for average pace to the right of average speed.
+
+df.insert(df.columns.get_loc("avgSpeedMPH") + 1, 'avgPaceMPM', 0)
+
+# Pace in minutes per mile is 60/MPH, only set it where avgSpeedMPH > 0.
 
 mask = df.avgSpeedMPH > 0
 df.loc[mask, 'avgPaceMPM'] = 60 / df.avgSpeedMPH
 
-# Filter columns to round to two decimals and replace 0 with NA
+# Filter columns to round to two and convert to nullable flaots with NA for 0.
+
 mask = [
     "distanceMiles",
     "avgSpeedMPH",
@@ -105,3 +108,5 @@ mask = [
 ]
 
 df[mask] = df[mask].round(2).convert_dtypes().replace(0.0, pd.NA)
+
+df.to_pickle("activities.pkl")
